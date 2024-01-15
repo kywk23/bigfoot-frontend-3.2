ef40f425ea5486b12dae5ceaabe0888935b8dd0f
kywk23 <kelvinywk.work@outlook.com>
2024-01-16 00:27:25 +0800

    first commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import logo from "/logo.png";
-
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [sightings, setSightings] = useState();
+
+  const handleGetSightings = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/sightings");
+      setSightings(response.data[0]);
+      console.log(sightings);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {}, [sightings]);
+
   return (
     <>
       <div>
         <img src={logo} className="logo react" alt="React logo" />
       </div>
       <h1>Bigfoot Frontend </h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <button onClick={handleGetSightings}>Get Sightings</button>
     </>
   );
 }
